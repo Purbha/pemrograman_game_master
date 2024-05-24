c053580b7ba166cd18d9643535b70f9b86c334af
Irsyad Purbha <irsyadpurbha@gmail.com>
2024-05-24 17:42:14 +0700

    UPDATE 20240524 17:00

---
+++ Pertemuan9/Assets/Scripts/BallController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    public int force;
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Vector2 arah = new Vector2(2,0).normalized;
+        rigid.AddForce(arah * force);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.name == "TepiKanan") 
+        {
+            Debug.Log("Tepi Kanan");
+            ResetBall();
+            Vector2 arah = new Vector2(2,0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if (coll.gameObject.name == "TepiKiri")
+        {
+            Debug.Log("Tepi Kiri");
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if(coll.gameObject.name == "Pemukul1" || coll.gameObject.name == "Pemukul2") {
+            Debug.Log("Pemukul");
+            float sudut = (transform.position.y - coll.transform.position.y) * 5f;
+            Vector2 arah = new Vector2(rigid.velocity.x, sudut).normalized;
+            rigid.velocity = new Vector2(0, 0);
+            rigid.AddForce(arah * force * 2);
+        }
+    }
+
+    void ResetBall()
+    {
+        transform.localPosition = new Vector2(0, 0);
+        rigid.velocity = new Vector2(0, 0);
+    }
+
+
+}
